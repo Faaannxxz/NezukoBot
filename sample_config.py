19cc552c4a134a9c94cd49090685d55d05c14a94
Pendragon <stayinherewthme@gmail.com>
2022-10-02 02:34:15 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,17 +5,17 @@ from dotenv import load_dotenv
 if path.exists("config.env"):
     load_dotenv("config.env")
 
-BOT_TOKEN = environ.get("BOT_TOKEN", None)
-API_ID = int(environ.get("API_ID", 6))
-API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
-LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
-GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
-MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
+BOT_TOKEN = environ.get("5701341223:AAHQL19XLCpyNXHtHmoLDMAvirFYEKZbahc", None)
+API_ID = int(environ.get("14314614", 6))
+API_HASH = environ.get("7f030d708946ee907bf0944c05c8cc09", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SUDO_USERS_ID = [int(x) for x in environ.get("5035563632", "").split()]
+LOG_GROUP_ID = int(environ.get("-1001729421519", None))
+GBAN_LOG_GROUP_ID = int(environ.get("-1001729421519", None))
+MESSAGE_DUMP_CHAT = int(environ.get("-1001729421519", None))
 WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
-MONGO_URL = environ.get("MONGO_URL", None)
+MONGO_URL = environ.get("mongodb+srv://maticnemanja:kucingkawin@cluster0.qhwwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", None)
 ARQ_API_URL = environ.get("ARQ_API_URL", None)
-ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
+ARQ_API_KEY = environ.get("KHJZGM-OXGZRB-WLJFAW-VKMQOV-ARQ", None)
 RSS_DELAY = int(environ.get("RSS_DELAY", None))
 UPSTREAM_REPO = environ.get(
     "UPSTREAM_REPO", "https://github.com/rozari0/NezukoBot.git"
